7c4390cf431c86c8ef1657cad03c766206a8e720
Afif Zafri <afif@redant.my>
2023-07-28 15:56:17 +0800

    Added loading state to the refresh button

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,30 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function fetchCatFact() {
+    toggleBtnIndicator('refresh-btn');
+
     fetch('https://catfact.ninja/fact')
         .then(response => response.json())
         .then(data => {
+            toggleBtnIndicator('refresh-btn');
             updateCatFact( data.fact );
         });
 }
 
 function updateCatFact( fact ) {
     document.getElementById('cat-fact-text').innerHTML = fact;
+}
+
+function toggleBtnIndicator( btn_class ) {
+    const btn = document.getElementsByClassName(btn_class)[0];
+
+    if(btn.getAttribute('disabled') != null) {
+        btn.disabled = false;
+        btn.querySelectorAll(`:scope ${'.indicator-label'}`)[0].classList.remove("d-none");
+        btn.querySelectorAll(`:scope ${'.indicator-progress'}`)[0].classList.add("d-none");
+    } else {
+        btn.disabled = true;
+        btn.querySelectorAll(`:scope ${'.indicator-label'}`)[0].classList.add("d-none");
+        btn.querySelectorAll(`:scope ${'.indicator-progress'}`)[0].classList.remove("d-none");
+    }
 }
